3d044b5d2ffd939308ba73a0b1fb1ea6fa187b0e
rittdhar12 <rittdhar12@gmail.com>
2024-02-07 19:56:49 -0500

    Updated next js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
 };
 
 {
     swcMinify: false; // it should be false by default
 }
-export default nextConfig;
+module.exports = nextConfig
